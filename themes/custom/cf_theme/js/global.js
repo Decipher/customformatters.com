9818d59b670729c0107bc97dfb9700fea97dfa2a
Stuart Clark <stu@rtclark.net>
2012-06-02 17:52:44 +1000

    Revert "Responsive images, Formatter example image filed and devel tweaks."
    This reverts commit ca6a14fd3481383895db4ff4ab3da8d871fc025c.

--- themes/custom/cf_theme/js/global.js
+++ themes/custom/cf_theme/js/global.js
@@ -27,11 +27,6 @@
           ? $(this).parents('.checkbox-wrapper').parent().addClass('checked')
           : $(this).parents('.checkbox-wrapper').parent().removeClass('checked');
       });
-
-      // Replace file inputs with custom, themeable element.
-      $('input[type="file"]:not(.processed)').each(function() {
-        $(this).addClass('processed').customFileInput();
-      });
     }
   }
 })(jQuery);
